e0b72f15ef0f28bbc8bf75ef5aae9f15a2cba758
Dharshan P <dhrsdharshan@gmail.com>
2026-02-18 14:27:34 +0530

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,17 @@
+#Working with NumPy Arrays
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([5,6,7,8])
+
+print("Array a:", a)
+print("Mean:", np.mean(a))
+print("Sum:", np.sum(a))
+print("Dot Product:", np.dot(a,b))
+
+
+Output
+Array a: [1 2 3 4]
+Mean: 2.5
+Sum: 10
+Dot Product: 70
